4600c5f1b2c5072596e0f741b6cc0a35af9a1b91
Mina Magdy <phoenix.mena99@gmail.com>
2022-12-28 20:31:34 +0200

    render 4 cards in devices.html

--- js/fillinfo.js
+++ js/fillinfo.js
@@ -15,4 +15,18 @@ fetch(`../data/${params.category}.json`).then(response => {
     for (feature of data[params.id].features) {
         document.querySelector(".product-features").innerHTML += `<li>${feature}</li>`;
     }
+    document.getElementById("multiCamera").src = data[params.id].cards[0].image;
+    document.getElementById("multiPen").src = data[params.id].cards[1].image;
+    document.getElementById("multiExcellence").src = data[params.id].cards[2].image;
+    document.getElementById("multiBuilt").src = data[params.id].cards[3].image;
+
+    document.getElementById("title_inf_camera").innerHTML = data[params.id].cards[0].name;
+    document.getElementById("title_inf_pen").innerHTML = data[params.id].cards[1].name;
+    document.getElementById("title_inf_Excellence").innerHTML = data[params.id].cards[2].name;
+    document.getElementById("title_inf_Built").innerHTML = data[params.id].cards[3].name;
+
+    document.getElementById("data_inf_camera").innerHTML = data[params.id].cards[0].description;
+    document.getElementById("data_inf_pen").innerHTML = data[params.id].cards[1].description;
+    document.getElementById("data_inf_Excellence").innerHTML = data[params.id].cards[2].description;
+    document.getElementById("data_inf_Built").innerHTML = data[params.id].cards[3].description;
 });
