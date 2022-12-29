b609af1b123df3ce34267fc179ed0f4c7f8a826d
MinaPhoenix <phoenix.mena99@gmail.com>
2022-12-29 14:56:10 +0200

    remove first slash

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,21 +3,21 @@ const data = [
         id: 1,
         title: 'QN900B Neo QLED 8K',
         description: "85'' Class QN900B Samsung Neo QLED 8K Smart TV (2022)",
-        imgPath: `../assets/images/tv/QN900B.webp`,
+        imgPath: `assets/images/tv/QN900B.webp`,
         link: "html/store.html?category=tv"
     },
     {
         id: 2,
         title: 'Laptop',
         description: 'Get A Laptop With 1 Year Warranty And Free Gifts With Every Purchase',
-        imgPath: `/assets/images/slide-show/${7}.jpg`,
+        imgPath: `assets/images/slide-show/${7}.jpg`,
         link: "html/store.html?category=laptops"
     },
     {
         id: 3,
         title: "HP 24mh FHD Monitor",
         description: "The HP 24mh FHD Monitor has all the style and performance you need in a monitor without breaking the bank. Experience more desk space than you thought possible with this height-adjustable ultra-slim HP monitor.",
-        imgPath: "/assets/images/accessories/monitor.png",
+        imgPath: "assets/images/accessories/monitor.png",
         link: "html/store.html?category=accessories"
         
     },
@@ -25,14 +25,14 @@ const data = [
         id: 4,
         title: "Airpods (3rd gen)",
         description: "With Adaptive EQ that tunes music to your ears, and longer battery life that charges up in a snap. It shrugs off sweat and sprinkles. Inward-facing microphones detect what you’re hearing. The force sensor gives you even more control over your entertainment. Press to play, pause, skip songs and answer and end calls.",
-        imgPath: "/assets/images/airpods/airpods_3.png",  
+        imgPath: "assets/images/airpods/airpods_3.png",  
         link: "html/store.html?category=airpods"     
     },
     {
         id: 5,
         title: "Apple Watch Series 4",
         description: "The Apple Watch Series 4 is a technological masterpiece.To begin with, this smartphone comes with the high-end S4 64-bit Dual-Core processor",
-        imgPath: "/assets/images/watches/watches_1341378.png ",  
+        imgPath: "assets/images/watches/watches_1341378.png ",  
         link: "html/store.html?category=watches"      
     },
     {
@@ -46,7 +46,7 @@ const data = [
         id: 7,
         title: "HUAWEI MatePad 11''",
         description: "Enjoy fluid work and play with 120 Hz HUAWEI FullView Display, Huawei Share and more.",
-        imgPath: "/assets/images/tablets/tablet1.3.jpg ",  
+        imgPath: "assets/images/tablets/tablet1.3.jpg ",  
         link: "html/store.html?category=tablets"      
     }
 ]
